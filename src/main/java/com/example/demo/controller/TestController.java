fff8f230778c8ab82a6db2cb104b2c3099d4534f
Andrea Verdiglione <andverdy@yahoo.it>
2022-04-09 10:02:21 +0200

    [demo] create test api

---
+++ src/main/java/com/example/demo/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.demo.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+@CrossOrigin
+public class TestController {
+	
+	@GetMapping("/getTest")
+	public String getSaluto(@RequestParam Integer numero) {
+		
+		return "ciao " + numero;
+	}
+
+}
